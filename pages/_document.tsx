cb1e3341b04b3f618f289b51b1c6cbc00d75ef91
nguyenngochieu <ngnohieu@gmail.com>
2023-05-16 13:26:40 +0700

    edit meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
    return (
       <Html lang="en">
-         <Head></Head>
+         <Head>
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Open Market - Super Ecommerce" />
+            <meta
+               property="og:description"
+               content="Quality leading E-commerce Website"
+            />
+            <meta
+               property="og:image"
+               content="https://res.cloudinary.com/ngnohieu/image/upload/v1684215390/thumbnailArtboard_19_lh1aia.png"
+            />
+         </Head>
          <body>
             <Main />
             <NextScript />
