3590f64f44bbe45e0d22a20a6d3abd7276366156
nguyenngochieu <ngnohieu@gmail.com>
2023-06-14 18:09:02 +0700

    fix 500

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const ServerError = () => {
+   const router = useRouter();
    return (
-      <Layout title={500}>
-         <div>500</div>
-         {/* <div className="relative overflow-hidden w-1/3 mx-auto mt-8">
-            <Image src={Image404} alt="404" className="object-cover" />
-         </div> */}
+      <Layout title="500 Error">
+         <div className="relative overflow-hidden w-1/3 aspect-square mx-auto mt-8">
+            <Image
+               src="https://res.cloudinary.com/ngnohieu/image/upload/v1686739645/12123Asset_1_hzpcxw.png"
+               alt="500"
+               layout="fill"
+               className="object-cover"
+            />
+         </div>
+         <button
+            className="text-center py-3 px-6 my-6 rounded-lg bg-primary-color text-white font-semibold hover:shadow-lg hover:shadow-primary-color hover:brightness-95"
+            onClick={() => router.push("/")}
+         >
+            Back to homepage
+         </button>
       </Layout>
    );
 };
